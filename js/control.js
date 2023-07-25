27161ca9405f059ffcf48d3f7fcc3080f4b10642
baitapai654321 <140496783+baitapai654321@users.noreply.github.com>
2023-07-25 16:11:58 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+ strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+ typeSpeed:50,
+ backSpeed:50,
+ loop:true,
+ });
+
+$('.counter').countUp();
+
+new WOW().init();
